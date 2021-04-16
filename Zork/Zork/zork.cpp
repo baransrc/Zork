7903ab72568086eaa4960348f42d9d0436030bde
Baran Surucu <baransrc@gmail.com>
2021-04-16 19:00:35 +0300

    Created the Visual Studio 2017 project for the game.

---
+++ Zork/Zork/zork.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "This project will become a Zork inspired game in the future commits!" << std::endl;
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
